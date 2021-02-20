686f54c879a30dcd41df91cea5ac15acd9527870
natashamaya <natashamaya_19@kharisma.ac.id>
2021-02-20 16:54:24 +0800

    add

--- jokes/application/config/config.php
+++ jokes/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://srv4.pweb201.local/';
+$config['base_url'] = 'https://51018014.p-web.click/jokes/';
 
 /*
 |--------------------------------------------------------------------------
